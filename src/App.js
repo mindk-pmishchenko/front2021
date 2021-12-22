e8409a008107370e9363836b2bc15d2bed0c9bd3
mindk-pmishchenko <007gold007>
2021-12-22 12:19:08 +0200

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import {useState} from 'react';
 import {UserProfileContainer} from "./containers/userProfile";
 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  const [string, setString] = useState('a');
+
+  const inc = () => {
+    setCounter(counter + 1);
+  }
+
+  const add = () => {
+    setString(string + string);
+    setCounter(counter + 1);
+  }
+
   return (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <UserProfileContainer firstName="first" lastName="last" middleName="middle" birthdayYear={1990} />
+      <UserProfileContainer firstName="first" lastName="last" middleName="middle" birthdayYear={1990} add={add} string={string}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={inc}>Click me UP!</button>
+        <div>count: {counter}</div>
+      </UserProfileContainer>
     </header>
   </div>
   );
--- src/containers/userProfile/index.js
+++ src/containers/userProfile/index.js
@@ -1,8 +1,14 @@
+import React from 'react';
 import {UserProfile} from "../../components/userProfile";
 
-export function UserProfileContainer({ firstName, lastName, middleName, birthdayYear }) {
+export function UserProfileContainer({ firstName, lastName, middleName, birthdayYear, add, string, children }) {
     const fullName = `${firstName} ${lastName} (${middleName})`;
     const age = 2021 - birthdayYear;
 
-    return <UserProfile fullName={fullName} age={age} />
+    return (<>
+        {children}
+        <UserProfile fullName={fullName} age={age} />
+        <button onClick={add}>Click me ADD!</button>
+        <div>string: {string}</div>
+    </>);
 }
